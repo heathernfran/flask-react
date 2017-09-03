9bda5d03cb32c619fa7501164aa8fe01878aec5e
Heather Frantisak <heathernfran@gmail.com>
2017-09-02 14:28:31 -1000

    Create app component

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack')
 var path = require('path')
 
 module.exports = {
-  entry: './components/index.jsx',
+  entry: './components/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'static')

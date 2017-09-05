883ff33384e05f0c69a2d4e701310a05ce84284f
Heather Frantisak <hfrantisak@mvnp.com>
2017-09-05 10:46:16 -1000

    Get location data, using launch library api

--- components/App.js
+++ components/App.js
@@ -1,11 +1,39 @@
-// 'use strict'
-
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+    }
+  }
+  
+  componentDidMount() {
+    return fetch('https://launchlibrary.net/1.2/location')
+            .then((response) => response.json())
+            .then((json) => {
+              console.log(json)
+              this.setState({
+                data: json.locations
+              })
+            })
+            .catch((error) => {
+              console.error(`Error in response: ${error}`)
+            })
+  }
+  
   render() {
     return (
-      <h1>App Component</h1>
+      <div>
+        <h1>Launch Library API</h1>
+        {this.state.data.map((value) => {
+          return (
+            <div key={value.id}>
+              {value.name}
+            </div>
+          )
+        })}
+      </div>
     )
   }
 }

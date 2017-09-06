0ac15a8fb8bf7b6e63b7f83e3406a309c91da15a
Heather Frantisak <hfrantisak@mvnp.com>
2017-09-05 15:22:38 -1000

    Use leaflet for map, implementing react components for leaflet

--- components/App.js
+++ components/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       data: [],
+      center: [28.44, -80.56],
+      zoom: 5,
     }
   }
   
-  componentDidMount() {
+  componentWillMount() {
     return fetch('https://launchlibrary.net/1.2/pad/launch%20complex')
             .then((response) => response.json())
             .then((json) => {
@@ -23,18 +26,36 @@ export default class App extends Component {
   }
   
   render() {
+    const styles = {
+      width: '100vw',
+      height: '100vh',
+      overflow: 'hidden'
+    }
     return (
       <div>
         <h1>Launch Library API</h1>
-        {this.state.data.map((value) => {
-          return (
-            <div key={value.id}>
-              {value.name}
-              Latitude: {value.latitude}
-              Longitude: {value.longitude}
-            </div>
-          )
-        })}
+        <div>
+          <Map
+            style={styles}
+            center={this.state.center}
+            zoom={this.state.zoom}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+            {this.state.data.map((value) => {
+              return (
+                <Marker 
+                  key={value.id}
+                  position={{lat: value.latitude}, {lng: value.longitude}}
+                  >
+                  <Popup>
+                    <span>{value.text}</span>
+                  </Popup>
+                </Marker>
+              )
+            })}
+          </Map>
+        </div>
       </div>
     )
   }
